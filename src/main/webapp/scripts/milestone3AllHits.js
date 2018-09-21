2c56dfdb45a8c61bcdeeb7187d85a76db55c609b
Shantanu-Mukherjee <37309507+Shantanu-Mukherjee@users.noreply.github.com>
2018-09-21 17:56:00 -0500

    commit

--- src/main/webapp/scripts/milestone3AllHits.js
+++ src/main/webapp/scripts/milestone3AllHits.js
@@ -12,3 +12,145 @@ function checkUIpathAccessFailure() {
 	callWebservice('/rest/login/checkUIpathAccess', data, null, null, null, 'application/x-www-form-urlencoded');
 }
 
+function alertsRemindersGrid() {
+	var otherParams = {
+		firstParam : 'firstParamValue',
+		secondParam : 'secondParamValue',
+		thirdParam : 'thirdParamValue',
+		fourthParam : 'fourthParamValue'
+	}
+	var gridComponent = { 
+			start : 0,
+			limit : 100,
+			otherParams : encodeObjectAsJSON(otherParams),
+			sorters : [
+				{
+					id : 'S1',
+					mapping : 'actionDateMillis',
+					columnName : 'Action Date',
+					order : 'ASC'
+				},
+				{
+					id : 'S2',
+					mapping : 'initiatedBy',
+					columnName : 'Initiated By',
+					order : 'DESC'
+				}
+			],
+			filters : [
+				{
+					id : 'F1',
+					type : 'string',
+					mapping : 'subject',
+					columnId : 'subjectColumn',
+					stringValue : 'search this text'
+				},
+				{
+					id : 'F2',
+					type : 'date',
+					mapping : 'initiatedDateMillis',
+					columnId : 'initiatedDateMillisColumn',
+					beforeDateMillis : (new Date()).getTime(),
+					onDateMillis : (new Date()).getTime(),
+					afterDateMillis : (new Date()).getTime()
+				}
+			]
+	}
+	callWebservice('/rest/employee/alertsRemindersGrid', encodeObjectAsJSON(gridComponent), null, null, null, 'application/json');
+}
+
+function tasksGrid() {
+	var otherParams = {
+		firstParam : 'firstParamValue',
+		secondParam : 'secondParamValue',
+		thirdParam : 'thirdParamValue',
+		fourthParam : 'fourthParamValue'
+	}
+	var gridComponent = { 
+			start : 0,
+			limit : 100,
+			otherParams : encodeObjectAsJSON(otherParams),
+			sorters : [
+				{
+					id : 'S1',
+					mapping : 'actionDateMillis',
+					columnName : 'Action Date',
+					order : 'ASC'
+				},
+				{
+					id : 'S2',
+					mapping : 'initiatedBy',
+					columnName : 'Initiated By',
+					order : 'DESC'
+				}
+			],
+			filters : [
+				{
+					id : 'F1',
+					type : 'string',
+					mapping : 'subject',
+					columnId : 'subjectColumn',
+					stringValue : 'search this text'
+				},
+				{
+					id : 'F2',
+					type : 'date',
+					mapping : 'initiatedDateMillis',
+					columnId : 'initiatedDateMillisColumn',
+					beforeDateMillis : (new Date()).getTime(),
+					onDateMillis : (new Date()).getTime(),
+					afterDateMillis : (new Date()).getTime()
+				}
+			]
+	}
+	callWebservice('/rest/employee/tasksGrid', encodeObjectAsJSON(gridComponent), null, null, null, 'application/json');
+}
+
+function workflowsGrid() {
+	var otherParams = {
+		firstParam : 'firstParamValue',
+		secondParam : 'secondParamValue',
+		thirdParam : 'thirdParamValue',
+		fourthParam : 'fourthParamValue'
+	}
+	var gridComponent = { 
+			start : 0,
+			limit : 100,
+			otherParams : encodeObjectAsJSON(otherParams),
+			sorters : [
+				{
+					id : 'S1',
+					mapping : 'actionDateMillis',
+					columnName : 'Action Date',
+					order : 'ASC'
+				},
+				{
+					id : 'S2',
+					mapping : 'initiatedBy',
+					columnName : 'Initiated By',
+					order : 'DESC'
+				}
+			],
+			filters : [
+				{
+					id : 'F1',
+					type : 'string',
+					mapping : 'subject',
+					columnId : 'subjectColumn',
+					stringValue : 'search this text'
+				},
+				{
+					id : 'F2',
+					type : 'date',
+					mapping : 'initiatedDateMillis',
+					columnId : 'initiatedDateMillisColumn',
+					beforeDateMillis : (new Date()).getTime(),
+					onDateMillis : (new Date()).getTime(),
+					afterDateMillis : (new Date()).getTime()
+				}
+			]
+	}
+	callWebservice('/rest/employee/workflowsGrid', encodeObjectAsJSON(gridComponent), null, null, null, 'application/json');
+}
+
+
